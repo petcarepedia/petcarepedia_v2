5f5164246c5d67ca41c4d589162cfd4cc5f2ac01
pc <pc@DESKTOP-7FP2JHV>
2023-05-19 01:13:43 +0900

    kang_add_notice(customer_notice)

--- petcarepedia/src/main/java/com/project/petcarepedia/NoticeController.java
+++ petcarepedia/src/main/java/com/project/petcarepedia/NoticeController.java
@@ -13,27 +13,39 @@ public class NoticeController {
 		return "/admin/notice/admin_notice";
 	}
 	
-	// admin_notice_content 관리자 공지사항 상세보기 페이지
+	// admin_notice_content.do 관리자 공지사항 상세보기 페이지
 	@RequestMapping(value="/admin_notice_content.do", method=RequestMethod.GET)
 	public String admin_notice_content() {
 		return "/admin/notice/admin_notice_content";
 	}
 	
-	//admin_notice_write 관리자 공지사항 작성 페이지
+	//admin_notice_write.do 관리자 공지사항 작성 페이지
 	@RequestMapping(value="/admin_notice_write.do", method=RequestMethod.GET)
 	public String admin_notice_write() {
 		return "/admin/notice/admin_notice_write";
 	}
 	
-	//admin_notice_delete 관리자 공지사항 삭제 페이지
+	//admin_notice_delete.do 관리자 공지사항 삭제 페이지
 	@RequestMapping(value="/admin_notice_delete.do", method=RequestMethod.GET)
 	public String admin_notice_delete() {
 		return "/admin/notice/admin_notice_delete";
 	}
 	
-	//admin_notice_update 관리자 공지사항 수정 페이지
+	//admin_notice_update.do 관리자 공지사항 수정 페이지
 	@RequestMapping(value="/admin_notice_update.do", method=RequestMethod.GET)
 	public String admin_notice_update() {
 		return "/admin/notice/admin_notice_update";
+	}
+	
+	//notice.do 사용자 공지사항 리스트
+	@RequestMapping(value="/notice.do", method=RequestMethod.GET)
+	public String notice() {
+		return "/notice/notice";
+	}
+	
+	//notice_content.do 사용자 공지사항 리스트
+	@RequestMapping(value="/notice_content.do", method=RequestMethod.GET)
+	public String notice_content() {
+		return "/notice/notice_content";
 	}
 }
--- petcarepedia/src/main/webapp/WEB-INF/views/notice/notice_content.jsp
+++ petcarepedia/src/main/webapp/WEB-INF/views/notice/notice_content.jsp
@@ -9,8 +9,7 @@
 </head>
 <body>
 	<!-- header -->
-	<iframe src="http://localhost:9000/mycgv/header.html" 
-				scrolling="no" width="100%" height="100px" frameborder="0"></iframe>
+	<jsp:include page="../header.jsp"></jsp:include>
 	<div class="content">
 		<section class="notice">
 			<div id="title_l">
@@ -29,12 +28,11 @@
 			</table>
 			<div id="date">2323/04/26 18:38</div>
 			<div class="nc_button_r">
-				<a href="notice.jsp"><button type="button" class="button">목록</button></a>
+				<a href="notice.do"><button type="button" class="button">목록</button></a>
 			</div>
 		</section>
 	</div>
 	<!-- footer -->
-	<iframe src="http://localhost:9000/mycgv/footer.html" 
-				scrolling="no" width="100%" height="400px" frameborder="0"></iframe>
+	<jsp:include page="../footer.jsp"></jsp:include>
 </body>
 </html>
