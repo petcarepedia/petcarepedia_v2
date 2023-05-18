f02db316aea9812714c26cd0ad6f2d30260358b3
TJ <TJ@DESKTOP-4H5QRU1>
2023-05-18 17:24:18 +0900

    Add HospitalVo

---
+++ petcarepedia/src/main/java/com/project/vo/HospitalVo.java
@@ -0,0 +1,87 @@
+package com.project.vo;
+
+public class HospitalVo {
+	int rno;
+	String hid, hname, gloc, loc, tel, htime, ntime, holiday, animal,intro, img, hrink;
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getHid() {
+		return hid;
+	}
+	public void setHid(String hid) {
+		this.hid = hid;
+	}
+	public String getHname() {
+		return hname;
+	}
+	public void setHname(String hname) {
+		this.hname = hname;
+	}
+	public String getGloc() {
+		return gloc;
+	}
+	public void setGloc(String gloc) {
+		this.gloc = gloc;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getHtime() {
+		return htime;
+	}
+	public void setHtime(String htime) {
+		this.htime = htime;
+	}
+	public String getNtime() {
+		return ntime;
+	}
+	public void setNtime(String ntime) {
+		this.ntime = ntime;
+	}
+	public String getHoliday() {
+		return holiday;
+	}
+	public void setHoliday(String holiday) {
+		this.holiday = holiday;
+	}
+	public String getAnimal() {
+		return animal;
+	}
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getHrink() {
+		return hrink;
+	}
+	public void setHrink(String hrink) {
+		this.hrink = hrink;
+	}
+	
+	
+}
