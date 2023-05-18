263ab0e873d6656fe74f301acd48ad0234f44bfc
kyj <willbackv@gmail.com>
2023-05-18 17:24:10 +0900

    Add BookingVo

---
+++ petcarepedia/src/main/java/com/project/vo/BookingVo.java
@@ -0,0 +1,51 @@
+package com.project.vo;
+
+public class BookingVo {
+	String bid, bdate, vdate, vitme, bstate, mid;
+	int rno;
+	
+	public String getBid() {
+		return bid;
+	}
+	public void setBid(String bid) {
+		this.bid = bid;
+	}
+	public String getBdate() {
+		return bdate;
+	}
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	public String getVdate() {
+		return vdate;
+	}
+	public void setVdate(String vdate) {
+		this.vdate = vdate;
+	}
+	public String getVitme() {
+		return vitme;
+	}
+	public void setVitme(String vitme) {
+		this.vitme = vitme;
+	}
+	public String getBstate() {
+		return bstate;
+	}
+	public void setBstate(String bstate) {
+		this.bstate = bstate;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	
+	
+}
