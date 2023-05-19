af33cc3208028aca102d794c2f64b7489eb37dcf
tj <tj@DESKTOP-4LM468H>
2023-05-19 12:19:10 +0900

    ⚡️Add LoginController - login.do

--- petcarepedia/src/main/java/com/project/petcarepedia/LoginController.java
+++ petcarepedia/src/main/java/com/project/petcarepedia/LoginController.java
@@ -1,5 +1,16 @@
 package com.project.petcarepedia;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class LoginController {
+	/**
+	 * login.do - ·Î±×ÀÎ
+	 */
+	@RequestMapping(value="/login.do",method=RequestMethod.GET)
+	public String login() {
+		return "/login/login";
+	}
 }
