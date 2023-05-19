a9302e2f3dcdb58188d809f857b264670218cb61
tj <tj@DESKTOP-4LM468H>
2023-05-19 12:47:01 +0900

    ⚡️Add JoinController

--- petcarepedia/src/main/java/com/project/petcarepedia/JoinController.java
+++ petcarepedia/src/main/java/com/project/petcarepedia/JoinController.java
@@ -1,5 +1,16 @@
 package com.project.petcarepedia;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class JoinController {
-	
+	/**
+	 * join.do - 회원가입
+	 */
+	@RequestMapping(value="/join.do",method=RequestMethod.GET)
+	public String join() {
+		return "/join/join";
+	}
 }
