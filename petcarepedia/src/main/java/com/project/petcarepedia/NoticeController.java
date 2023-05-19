c6514733c326cbcbdd226065c845a52aced411a0
tj <tj@DESKTOP-80J6U9U>
2023-05-19 18:25:33 +0900

    ⚡️ kang/notice_connetion_DB

--- petcarepedia/src/main/java/com/project/petcarepedia/NoticeController.java
+++ petcarepedia/src/main/java/com/project/petcarepedia/NoticeController.java
@@ -1,22 +1,46 @@
 package com.project.petcarepedia;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.project.dao.NoticeDao;
+import com.project.vo.NoticeVo;
 
 @Controller
 public class NoticeController {
 	
 	// admin_norice.do 관리자 공지사항 리스트 페이지
 	@RequestMapping(value="/admin_notice.do", method=RequestMethod.GET)
-	public String admin_notice() {
-		return "/admin/notice/admin_notice";
+	public ModelAndView admin_notice() {
+		ModelAndView model = new ModelAndView();
+		NoticeDao noticeDao = new NoticeDao();
+		ArrayList<NoticeVo> noticeList = noticeDao.select();
+		
+		model.addObject("noticeList", noticeList);
+		model.setViewName("/admin/notice/admin_notice");
+		
+		return model;
 	}
 	
 	// admin_notice_content.do 관리자 공지사항 상세보기 페이지
 	@RequestMapping(value="/admin_notice_content.do", method=RequestMethod.GET)
-	public String admin_notice_content() {
-		return "/admin/notice/admin_notice_content";
+	public ModelAndView admin_notice_content(String nid) {
+		ModelAndView model = new ModelAndView();
+		NoticeDao noticeDao = new NoticeDao();
+		NoticeVo noticeVo = noticeDao.select(nid);
+		
+		if(noticeVo != null) {
+			// 조회수 업데이트 DB
+			noticeDao.updateHits(nid);
+		}
+		model.addObject("noticeVo", noticeVo);
+		model.setViewName("/admin/notice/admin_notice_content");
+		
+		return model;
 	}
 	
 	//admin_notice_write.do 관리자 공지사항 작성 페이지
@@ -25,17 +49,50 @@ public class NoticeController {
 		return "/admin/notice/admin_notice_write";
 	}
 	
+	//admin_notice_write_proc.do 관리자 공지사항 작성 처리
+	@RequestMapping(value="/admin_notice_write_proc.do", method=RequestMethod.POST)
+	public String admin_notice_write_proc(NoticeVo noticeVo) {
+		String viewName = "";
+		NoticeDao noticeDao = new NoticeDao();
+		int result = noticeDao.insert(noticeVo);
+		if(result == 1) {
+			viewName = "redirect:/admin_notice.do";
+		}
+		else {
+			//에러 처리
+		}
+		return viewName;
+	}
+	
+	
+	
+	//admin_notice_update.do 관리자 공지사항 수정 페이지
+	@RequestMapping(value="/admin_notice_update.do", method=RequestMethod.GET)
+	public ModelAndView admin_notice_update(String nid) {
+		ModelAndView model = new ModelAndView();
+		
+		NoticeDao noticeDao = new NoticeDao();
+		NoticeVo noticeVo = noticeDao.select(nid);
+		/*
+		 * if(boardVo != null) { // 조회수 업데이트 DB boardDao.updateHits(bid); }
+		 */
+		model.addObject("nvo", noticeVo);
+		model.setViewName("/admin/notice/admin_notice_update");
+		
+		return model;
+	}
+	
+	
+	
+	
+	
+	
 	//admin_notice_delete.do 관리자 공지사항 삭제 페이지
 	@RequestMapping(value="/admin_notice_delete.do", method=RequestMethod.GET)
 	public String admin_notice_delete() {
 		return "/admin/notice/admin_notice_delete";
 	}
 	
-	//admin_notice_update.do 관리자 공지사항 수정 페이지
-	@RequestMapping(value="/admin_notice_update.do", method=RequestMethod.GET)
-	public String admin_notice_update() {
-		return "/admin/notice/admin_notice_update";
-	}
 	
 	//notice.do 사용자 공지사항 리스트
 	@RequestMapping(value="/notice.do", method=RequestMethod.GET)
--- petcarepedia/src/main/webapp/WEB-INF/views/admin/notice/admin_notice_write.jsp
+++ petcarepedia/src/main/webapp/WEB-INF/views/admin/notice/admin_notice_write.jsp
@@ -6,6 +6,8 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <link rel="stylesheet" href="http://localhost:9000/petcarepedia/css/kang_style.css">
+<script src="http://localhost:9000/petcarepedia/js/jquery-3.6.4.min.js"></script>
+<script src="http://localhost:9000/petcarepedia/js/petcarepedia_jsp_jquery_kang.js"></script>
 </head>
 <body>
 	<!-- header -->
@@ -15,21 +17,21 @@
 			<div id="title_l">
 				<h1 class="title">관리자 - 공지사항</h1>
 			</div>
-			<form name="writeForm" action="#"  method="get">
+			<form name="writeForm" action="admin_notice_write_proc.do"  method="post">
 				<table class="notice_content">
 					<tr>
 						<td>
-							<input type="text" name="ntitle">
+							<input type="text" name="title" id="title">
 						</td>
 					</tr>
 					<tr>
 						<td>
-							<textarea name="ncontent" class="ncontent"></textarea>
+							<textarea name="ncontent" class="ncontent" id="ncontent"></textarea>
 						</td>
 					</tr>					
 				</table>
 					<div class="nw_button_r">
-						<button type="button" class="ad_button">등록</button>
+						<button type="button" class="ad_button" id="btnNTW">등록</button>
 						<a href="admin_notice.do"><button type="button" class="ad_button">취소</button></a>
 					</div>
 			</form>
