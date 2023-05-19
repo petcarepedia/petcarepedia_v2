ac28b2190328c9ff4665b9728142e37ad841032d
kyj <willbackv@gmail.com>
2023-05-19 10:29:37 +0900

    Add Select(String mid)

--- petcarepedia/src/main/java/com/project/dao/BookingDao.java
+++ petcarepedia/src/main/java/com/project/dao/BookingDao.java
@@ -58,6 +58,37 @@ public class BookingDao extends DBConn {
 
 		return list;
 	} // ArrayList<BookingVo> select()
+	
+	
+	/** select(mid) - 로그인 후 예약확인하기 **/
+	public BookingVo select(String mid) {
+		BookingVo bookingVo = new BookingVo();
+		
+		String sql= "SELECT  ROWNUM RNO, BID, to_char(BDATE, 'yyyy-mm-dd') BDATE,  to_char(BDATE, 'yyyy-mm-dd') VDATE, VTIME, BSTATE, MID " + 
+				" FROM (SELECT * FROM PCP_BOOKING ORDER BY BID DESC)" + 
+				" WHERE MID=?";
+		getPreparedStatement(sql);
+		
+		try {
+			pstmt.setString(1, mid);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				bookingVo.setRno(rs.getInt(1));
+				bookingVo.setBid(rs.getString(2));
+				bookingVo.setBdate(rs.getString(3));
+				bookingVo.setVdate(rs.getString(4));
+				bookingVo.setVtime(rs.getString(5));
+				bookingVo.setBstate(rs.getString(6));
+				bookingVo.setMid(rs.getString(7));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return bookingVo;
+	} // select(String mid)
 
 	
 	/** update - 예약 수정하기 **/
