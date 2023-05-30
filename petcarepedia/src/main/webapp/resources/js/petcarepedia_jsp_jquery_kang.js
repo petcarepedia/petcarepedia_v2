3e3825a097ec03bd032b239797db1c1770b0650c
tj <tj@DESKTOP-80J6U9U>
2023-05-30 17:06:44 +0900

    kang/review_update

--- petcarepedia/src/main/webapp/resources/js/petcarepedia_jsp_jquery_kang.js
+++ petcarepedia/src/main/webapp/resources/js/petcarepedia_jsp_jquery_kang.js
@@ -71,11 +71,19 @@ $(document).ready(function(){
 			}  
 		}
 	});
+	
+	if($("#count").val() == 0 ) {
+		return false;
+	}
+	else {
+		$('html, body').animate({
+			scrollTop: $('#filter_lo').offset().top
+		}, 'slow');
+	}
 
+	
 
-
-
-
+	
 }); //ready
 
 
